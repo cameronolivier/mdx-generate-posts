ccad7057f206dc943bbbe66cad91c7c68b7f35f6
Cameron Olivier <cameronolivier@gmail.com>
2023-07-23 18:46:23 +0200

    feat: adding initial converter from csv to mdx

---
+++ index.js
@@ -0,0 +1,46 @@
+const inquirer = require('inquirer');
+const csv = require('csv-parser');
+const fs = require('fs-extra');
+const matter = require('gray-matter');
+
+async function run() {
+  const { csvPath, destination } = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'csvPath',
+      message: 'Enter the path to the CSV file:',
+      validate: async (value) => {
+        if (!value.length) {
+          return 'A valid file path is required.';
+        }
+        try {
+          await fs.access(value);
+          return true;
+        } catch {
+          return 'File not found at given path.';
+        }
+      },
+    },
+    {
+      type: 'input',
+      name: 'destination',
+      message: 'Enter the destination folder:',
+      validate: (value) => !!value.length || 'A valid folder name is required.',
+    },
+  ]);
+
+  const rows = [];
+  fs.createReadStream(csvPath)
+    .pipe(csv())
+    .on('data', (row) => rows.push(row))
+    .on('end', () => {
+      rows.forEach((row, idx) => {
+        const fileName = `${destination}/${idx + 1}.mdx`;
+        const content = matter.stringify('', row);
+        fs.outputFile(fileName, content);
+      });
+      console.log('MDX files have been successfully generated!');
+    });
+}
+
+run();
